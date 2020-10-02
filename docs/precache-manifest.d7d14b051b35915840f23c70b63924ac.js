ecc0032bf078ca1dcd2bd6fe1682859264e84869
Ulises Cipŕes <ulisescipresm@gmail.com>
2020-10-02 14:00:59 -0500

    docs agregado

---
+++ docs/precache-manifest.d7d14b051b35915840f23c70b63924ac.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2232cb4847657ea8efef8a71af4b6dcd",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7f647ff2117f401cfd12",
+    "url": "/static/css/main.7e687614.chunk.css"
+  },
+  {
+    "revision": "85a4a86f3fe06f17fcc7",
+    "url": "/static/js/2.8249eaeb.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.8249eaeb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7f647ff2117f401cfd12",
+    "url": "/static/js/main.231161c1.chunk.js"
+  },
+  {
+    "revision": "daee87f482498cdf724c",
+    "url": "/static/js/runtime-main.29242b1d.js"
+  }
+]);
